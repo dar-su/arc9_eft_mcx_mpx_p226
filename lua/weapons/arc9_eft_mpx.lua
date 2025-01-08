6ffaebe6b07b5cfe5928e625f6d544159622d650
Darsu <95997959+dar-su@users.noreply.github.com>
2025-01-09 04:45:46 +0700

    a

--- lua/weapons/arc9_eft_mpx.lua
+++ lua/weapons/arc9_eft_mpx.lua
@@ -63,7 +63,7 @@ SWEP.Firemodes = {
 SWEP.Slot = 2
 
 -- this thing can one hand sprint
-local handupang, handuppos, handupholdtype = Angle(-2, 40, -7), Vector(1, -5, -6), "normal"
+local handupang, handuppos, handupholdtype = Angle(-2, 40, -7), Vector(0, -4, -10), "normal"
 SWEP.OneHandedSprint = true
 SWEP.OneHandedSprintHook = function(self, old) if self:GetValue("HasStock") then return false end end
 SWEP.SprintAngHook = function(self, old) if !self:GetValue("HasStock") then return handupang end end
