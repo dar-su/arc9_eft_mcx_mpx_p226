ff69a956378741632166e1773cf1cf223800d875
Darsu <95997959+dar-su@users.noreply.github.com>
2025-04-15 20:05:58 +0700

    gooder pistol holster

--- lua/weapons/arc9_eft_p226r.lua
+++ lua/weapons/arc9_eft_p226r.lua
@@ -54,10 +54,11 @@ SWEP.CustomizeRotateAnchor = Vector(21.6, -4.28, -5.23)
 
 SWEP.NonTPIKAnimReload = ACT_HL2MP_GESTURE_RELOAD_REVOLVER
 
+SWEP.HoldTypeHolstered = "normal"
+
 if GetConVar("arc9_eft_nontpik_mode"):GetBool() then -- pistols
     SWEP.HoldType = "revolver"
     SWEP.HoldTypeSprint = "normal"
-    SWEP.HoldTypeHolstered = "normal"
     SWEP.HoldTypeSights = "revolver"
     SWEP.HoldTypeCustomize = "passive"
 end
